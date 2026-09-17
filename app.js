80174c8c53f6367e996f44c096d337fe0d656bfe
Natnael Gebretsadik <natigebru1@gmail.com>
2026-09-17 12:05:45 +0300

    Initial commit using express rather than OG http module

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+dotenv.config();
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const port1 = process.env.EXPRESS_PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app
+  .route("/contact")
+  .get((req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "public", "contactUs.html"));
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    res.status(201).sendFile(path.join(__dirname, "public", "404.html"));
+  });
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+app.listen(port1, () => {
+  console.log(`Server running on port ${port1}`);
+  console.log(`http://localhost:${port1}/`);
+});
